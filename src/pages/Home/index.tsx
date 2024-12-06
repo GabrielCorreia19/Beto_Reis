08810be71bdb4834045ae82b4996781433c3d401
Gabriel Correia <gabriel.correia@suporterenova.onmicrosoft.com>
2024-12-06 09:41:20 -0300

    feat: finalizando Home Page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,17 +1,24 @@
 import React, { useEffect } from "react";
 import "./style.css";
+import { MdVerified } from "react-icons/md";
+
 
 export default function Home() {
   return (
     <div className="home">
-      <div className="header-img">
-        <div className="section-img">
-          <img src="" alt="" />
+      <div className="header-perfil">
+      <div className="section-img">
+          <img
+            src="https://media-for2-1.cdn.whatsapp.net/v/t61.24694-24/361360914_279215728132783_6241601325352706502_n.jpg?stp=dst-jpg_tt6&ccb=11-4&oh=01_Q5AaIDLM6Xi5Sq8xC239AhbDfxICAFIs6ln1CVAaN089AZTg&oe=6763FCDA&_nc_sid=5e03e0&_nc_cat=105"
+            alt="foto-perfil"
+          />
         </div>
-        <div className="section-name">
-          <h3>Beto Reis</h3>
-          <span>@_obetoreis</span>
+        <div className="section-column">
+        <h3>Beto Reis</h3>
+        <span>@_obetoreis</span>
         </div>
+        <MdVerified color="blue" size={'20'}/>
+
       </div>
 
       <div className="card-cursos">
