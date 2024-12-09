3a92d6fc92726e86a6c83419956ac5aa34452d55
Gabriel Correia <gabriel.correia@suporterenova.onmicrosoft.com>
2024-12-09 09:32:49 -0300

    fix: corrigindo texto da tela 404

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -13,7 +13,7 @@ export default function Apresentacao() {
         <div className="section-column">
         <h1>404</h1>
         <h2>Página não encontrada</h2>
-        <p>Parece que você não conseguimos encontrar o que você está procurando.</p>
+        <p>Parece que não conseguimos encontrar o que você está procurando.</p>
         </div>
       <button onClick={navigateToHome}>VOLTAR PARA A HOME</button>
     </div>
